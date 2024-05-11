2f91b2fccf423dfc93e3e72c25c070b88f17a751
Ivan Vorobyov <ivanvorobyov1992@yandex.ru>
2024-05-11 18:00:58 +0300

    feat(routes): add routes and components

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,29 @@
-import { Routes } from '@angular/router';
+import { Routes } from '@angular/router'
+import { BaseComponent } from './components/base/base.component'
+import { ProductsComponent } from './components/products/products.component'
+import { ProductDetailsComponent } from './components/product-details/product-details.component'
+import { BasketComponent } from './components/basket/basket.component'
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {
+    path: '',
+    component: BaseComponent
+  },
+
+  {
+    path: 'products',
+    component: ProductsComponent
+  },
+
+  {
+    path: 'products/:id',
+    component: ProductDetailsComponent
+  },
+
+  {
+    path: 'basket',
+    component: BasketComponent
+  },
+
+  { path: '**', redirectTo: '', component: BaseComponent }
+]
