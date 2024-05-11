dcbbf9fa8c368e6aea234befa86188d7a10042db
Ivan Vorobyov <ivanvorobyov1992@yandex.ru>
2024-05-11 21:19:04 +0300

    feat(navbar): add nav component and add styles

--- src/app/components/ui/header/header.component.ts
+++ src/app/components/ui/header/header.component.ts
@@ -2,12 +2,13 @@ import { Component } from '@angular/core'
 import { MatToolbarModule } from '@angular/material/toolbar'
 import { MatIcon } from '@angular/material/icon'
 import { RouterLink } from '@angular/router'
+import { NavbarComponent } from '../navbar/navbar.component'
 
 
 @Component({
   selector: 'app-header',
   standalone: true,
-  imports: [MatToolbarModule, MatIcon, RouterLink],
+  imports: [MatToolbarModule, MatIcon, RouterLink, NavbarComponent],
   templateUrl: './header.component.html',
   styleUrl: './header.component.scss'
 })
