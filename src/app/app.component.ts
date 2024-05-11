50927644ea49fe32731fa747936e3b9c7a50e53e
Ivan Vorobyov <ivanvorobyov1992@yandex.ru>
2024-05-11 18:39:53 +0300

    feat(header): add base styles

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,16 @@
-import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { Component } from '@angular/core'
+import { RouterOutlet } from '@angular/router'
+import { HeaderComponent } from './components/ui/header/header.component'
+import { FooterComponent } from './components/ui/footer/footer.component'
+
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, HeaderComponent, FooterComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
 export class AppComponent {
-  title = 'online-shop-angular';
+  title = 'online-shop-angular'
 }
--- src/app/components/ui/header/header.component.ts
+++ src/app/components/ui/header/header.component.ts
@@ -1,9 +1,13 @@
-import { Component } from '@angular/core';
+import { Component } from '@angular/core'
+import { MatToolbarModule } from '@angular/material/toolbar'
+import { MatIcon } from '@angular/material/icon'
+import { RouterLink } from '@angular/router'
+
 
 @Component({
   selector: 'app-header',
   standalone: true,
-  imports: [],
+  imports: [MatToolbarModule, MatIcon, RouterLink],
   templateUrl: './header.component.html',
   styleUrl: './header.component.scss'
 })
